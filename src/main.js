437f4379cbecf19776ad496993cf1982836b520b
Jimmy Morales <jimmy.morales@taskeasy.com>
2019-06-09 23:52:31 -0600

    Add user pool setup with amplify

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import { RestDataSource } from './restDataSource'
 import VueSwal from 'vue-swal'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
 
 Vue.config.productionTip = false
 Vue.use(VueSwal)
+Vue.use(AmplifyPlugin, AmplifyModules)
 new Vue({
   render: function (h) { return h(App) },
   data: {
