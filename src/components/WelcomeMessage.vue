3c140dee5aeeba81ea6c4389dac5aaa252c5b1e7
Luis Detlefsen <luis.detlefsen@taskeasy.com>
2019-06-16 00:59:41 -0600

    Temp fix layout for small screens.

--- src/components/WelcomeMessage.vue
+++ src/components/WelcomeMessage.vue
@@ -5,13 +5,18 @@
       <img src="../assets/img/bg-landing1.svg" alt class="landing-1">
     </div>
     <div class="row justify-content-center">
-      <div class="landing-2 justify-content-center">
+      <!-- <div class="landing-2"> -->
+      <div class="landing-2">
+        <img
+          src="../assets/img/bg-landing2.svg"
+          alt
+          style="display:block;position:absolute;max-width:100%;max-height:auto;"
+        >
         <h1>¿Cómo surgió esta iniciativa?</h1>
 
-            <p
-              class="short-description justify-content-center"
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+        <p
+          class="short-description justify-content-center"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
     <div class="row">
@@ -40,7 +45,6 @@
             <button
               type="button"
               class="ml-2 btn btn-purple btn-lg btn-huge"
-              style="font-weight:bold;"
               v-on:click="goViewPromises"
             >Ver Promesas</button>
           </div>
@@ -73,14 +77,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-.btn-logo-huge{
+.btn-logo-huge {
   min-height: 244px;
 }
 .landing-1 {
   min-width: 100%;
   max-width: 100%;
-  height:auto;
+  height: auto;
   // max-height:50px;
 }
 .landing-2 > h1 {
@@ -88,19 +91,16 @@ export default {
   font-weight: bold;
 }
 .landing-2 {
+  display:contents;
   background: url(../assets/img/bg-landing2.svg) no-repeat;
   background-position: center;
   min-width: 100%;
   max-width: 100%;
   min-height: 354px;
   max-height: auto;
-
-  // min-width: 100%;
-  // max-width: 100%;
-  // background-image: url('../assets/img/promesa1.svg') no-repeat;
 }
 h1 {
-  padding-top: 1px;
+  // padding-top: 1px;
 }
 
 .card {
@@ -109,17 +109,27 @@ h1 {
   font-weight: normal;
 }
 .short-description {
-  max-width: 50%;
+  max-width: 80%;
 }
 
+.card-footer,
+button {
+  font-weight: bold;
+}
+.card {
+  margin-top: 15px;
+}
 @media only screen and (max-width: 768px) {
-  .card{
-    margin-top:15px;
-  }
-
   .landing-1 {
     max-width: 100%;
-    max-height: (40vw);
+    max-height: (29vw);
+  }
+
+  .landing-2 {
+    display: contents;
+    max-width: 100%;
+    max-height: 300px;
+    // margin-top:-10px;
   }
 }
 </style>
